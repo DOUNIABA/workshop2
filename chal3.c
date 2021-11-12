3781a81242493c4864f0c2be1268c41872dcdedd
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-12 15:40:56 +0100

    ajout

--- chal3.c
+++ chal3.c
@@ -6,13 +6,14 @@ int main()
 int nbr;
 printf("saisir un nombre:\n");
 scanf("%d",&nbr);
+if(nbr==0){
+    printf("non valide \n");
+}
 if(nbr%2==0){
     printf("le nombre %d est pair \n",nbr);
 }
 else if (nbr%2==1){
     printf("le nombre %d est impair \n",nbr);
 }
-
-    return 0;
+  return 0;
 }
- 
