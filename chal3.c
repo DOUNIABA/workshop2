3e76fda607b662326752848e7db474f0fd1be483
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-12 14:55:11 +0100

    ajout

---
+++ chal3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+int nbr;
+printf("saisir un nombre:\n");
+scanf("%d",&nbr);
+if(nbr%2==0){
+    printf("le nombre %d est pair \n",nbr);
+}
+else if (nbr%2==1){
+    printf("le nombre %d est impair \n",nbr);
+}
+
+    return 0;
+}
+ 
